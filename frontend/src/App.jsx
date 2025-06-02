6c0e765adc7beacd96a3129125423e22b290aa9a
Jeeere <jere.t.leukkunen@gmail.com>
2025-06-03 01:16:01 +0300

    Change to using original title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,10 +38,10 @@ function App() {
     <>
       <div className="movies">
         {movies.map((movie) => (
-          <div className="movie" key={movie.Name}>
+          <div className="movie" key={movie.OriginalTitle}>
             {movie.ExternalUrls && movie.ExternalUrls[1] && (
             <a href={movie.ExternalUrls[1]["Url"]} target="_blank" rel="noopener noreferrer">
-              <h2>{movie.Name}</h2>
+              <h2>{movie.OriginalTitle}</h2>
             </a>
           )}
           </div>
